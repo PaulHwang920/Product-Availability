5e453168bf6eb89ffd8f3df6d796cc7aabcaf830
PaulHwang920 <paul17@vt.edu>
2020-04-24 15:48:57 -0400

    date and time

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
 import time
+import datetime
+
+now = datetime.datetime.now()
 
 while(True):
+    print ("----- " + now.strftime("%Y-%m-%d %H:%M:%S") + " -----")
     os.system("python bestbuy.py")
     os.system("python target.py")
     os.system("python walmart.py")
