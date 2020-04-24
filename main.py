7d5534d3f8f7bbbf874060e91b7068b493a5bced
PaulHwang920 <paul17@vt.edu>
2020-04-24 15:52:00 -0400

    updates time

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import os
 import time
 import datetime
 
-now = datetime.datetime.now()
-
 while(True):
+    now = datetime.datetime.now()
     print ("----- " + now.strftime("%Y-%m-%d %H:%M:%S") + " -----")
     os.system("python bestbuy.py")
     os.system("python target.py")
